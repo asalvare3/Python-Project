d41fcc4cc62b584ae8ec1c5e0ea7e41eb0eff974
Christopher Frost <ChristopherWFrost@gmail.com>
2018-04-23 20:21:26 -0400

    Updated to grab mentions within restrictions.

--- main.py
+++ main.py
@@ -1,11 +1,29 @@
 import tweepy
+import time
 from credentials import *
+
+'''
+class MyStreamListener(tweepy.StreamListener):
+    def on_error(self, status_code):
+        if status_code == 420:
+            #returning False in on_error disconnects the stream
+            return False
+            # returning non-False reconnects the stream, with backoff.
+    
+    def on_status(self, status):
+        print(status.text)
+'''
 
 # Set up OAuth and integrate with API
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth)
 
-# Write a tweet to push to our Twitter account
-tweet = 'Hello, world!'
-api.update_status(status=tweet)
+#stream = MyStreamListener()
+#myStream = tweepy.Stream(auth = api.auth, listener=stream)
+
+while True:
+    since_id = 0
+    result = api.mentions_timeline()
+    print(result)
+    time.sleep(12)
