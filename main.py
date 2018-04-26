62e77da8d13e8191642e45f775e64273506aa2c9
vamato9 <vinamato9@gmail.com>
2018-04-25 20:31:42 -0400

    added function to download image

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import tweepy
 import time
+import urllib.request
 from credentials import *
 
 # Set up OAuth and integrate with API
@@ -7,11 +8,11 @@ auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth)
 
-#stream = MyStreamListener()
-#myStream = tweepy.Stream(auth = api.auth, listener=stream)
+# stream = MyStreamListener()
+# myStream = tweepy.Stream(auth = api.auth, listener=stream)
 
 while True:
-    since_id = 988883933889728511 #change to read from a config file
+    since_id = 988883933889728511  # change to read from a config file
     result = api.mentions_timeline(since_id)
 
     with open('latestMentions.txt', 'w') as f:
@@ -22,7 +23,7 @@ while True:
     with open('latestMentions.txt', 'r') as f:
         line = f.readline()
         line.split('Status')
-        
+
         # For each tweet...
         # Find new ID to update since_id
         line = line.split(' ')
@@ -30,5 +31,12 @@ while True:
         # Grab image
 
         # Call image processor
+
+        def dwn_image(result):
+            image = result.entities.get('media', [])
+            if(len(image) > 0):
+                # wget.download(image)
+                urllib.request.urlretrieve(image, '/home/vamato9/Documents/School/Python/Python_Project/Images')
+
         # Reply
     time.sleep(12)
