dd9bdcd84be423113fdedfbbf08016c53adc2885
Christopher Frost <ChristopherWFrost@gmail.com>
2018-04-25 20:25:55 -0400

    update.

--- main.py
+++ main.py
@@ -2,18 +2,6 @@ import tweepy
 import time
 from credentials import *
 
-'''
-class MyStreamListener(tweepy.StreamListener):
-    def on_error(self, status_code):
-        if status_code == 420:
-            #returning False in on_error disconnects the stream
-            return False
-            # returning non-False reconnects the stream, with backoff.
-    
-    def on_status(self, status):
-        print(status.text)
-'''
-
 # Set up OAuth and integrate with API
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
@@ -23,7 +11,24 @@ api = tweepy.API(auth)
 #myStream = tweepy.Stream(auth = api.auth, listener=stream)
 
 while True:
-    since_id = 0
-    result = api.mentions_timeline()
-    print(result)
+    since_id = 988883933889728511 #change to read from a config file
+    result = api.mentions_timeline(since_id)
+
+    with open('latestMentions.txt', 'w') as f:
+        for row in result:
+            f.write("%s\n" % str(row))
+
+    # Process latest mentions
+    with open('latestMentions.txt', 'r') as f:
+        line = f.readline()
+        line.split('Status')
+        
+        # For each tweet...
+        # Find new ID to update since_id
+        line = line.split(' ')
+        since_id = line[12][0:-1]
+        # Grab image
+
+        # Call image processor
+        # Reply
     time.sleep(12)
