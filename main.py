e2e14d757bcbcaf757e6d085316e3a559e9edb1e
Christopher Frost <ChristopherWFrost@gmail.com>
2018-04-27 12:48:12 -0400

    download images works

--- main.py
+++ main.py
@@ -3,43 +3,36 @@ import time
 import urllib.request
 from credentials import *
 
+
 # Set up OAuth and integrate with API
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth)
 
 
-def dwn_image(result):
-    image = result.entities.get('media', [])
-    if(len(image) > 0):
-        # wget.download(image)
-        urllib.request.urlretrieve(image, '/home/vamato9/Documents/School/Python/Python_Project/Images')
-# stream = MyStreamListener()
-# myStream = tweepy.Stream(auth = api.auth, listener=stream)
-
-
 while True:
-    since_id = 988883933889728511  # change to read from a config file
+    with open('since_id.txt') as i:
+        since_id = i.readline().strip()
     result = api.mentions_timeline(since_id)
 
+    # Left in for debug purposes. Really not necessary to the script.
     with open('latestMentions.txt', 'w') as f:
         for row in result:
             f.write("%s\n" % str(row))
 
-    # Process latest mentions
-    with open('latestMentions.txt', 'r') as f:
-        line = f.readline()
-        line.split('Status')
-
-        # For each tweet...
+    # For each tweet...
+    for tweet in result:
+        
         # Find new ID to update since_id
-        line = line.split(' ')
-        since_id = line[12][0:-1]
-        # Grab image
+        since_id = tweet.id
+        with open('since_id.txt', 'w') as o:
+            o.write(str(since_id))
+
+        # Grab image URL and download
+        if 'media' in tweet.entities:
+            for image in tweet.entities['media']:
+                urllib.request.urlretrieve(image['media_url'], '/Users/ChristopherWFrost/Developer/Git/Python-Project/temp.jpg')
 
         # Call image processor
-
-        dwn_image(line)
-
         # Reply
     time.sleep(12)
